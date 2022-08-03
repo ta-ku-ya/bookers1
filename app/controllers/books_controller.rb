b9c5f529cb26a5bb43d9cccc509f7ccea59fe92f
ta-ku-ya <108854454+ta-ku-ya@users.noreply.github.com>
2022-08-03 13:02:44 +0000

    [add]showのリンク

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -13,12 +13,19 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+    
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get '/top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
+  get 'top' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
